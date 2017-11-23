8e44b33182ff67b41f7b64a3e098f89f1a55dde1
Piyush Mehndiratta <piyushmehndiratta@Piyushs-MacBook-Air.local>
2017-11-24 00:59:28 +0530

    lol

--- app/src/main/java/com/the/project/quotes/Main2Activity.java
+++ app/src/main/java/com/the/project/quotes/Main2Activity.java
@@ -2,15 +2,22 @@ package com.the.project.quotes;
 
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,38 +30,60 @@ import github.nisrulz.screenshott.ScreenShott;
 public class Main2Activity extends AppCompatActivity  {
 private TextView title;
 private TextView quote;
-private FloatingActionButton bb;
+private Button bb;
 private LinearLayout linearLayout;
 private TextView name;
 private Bitmap bitmap;
+private Button submit;
     private final static String[] requestWritePermission =
             { Manifest.permission.WRITE_EXTERNAL_STORAGE };
+
+    String a,b,c,d;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        getSupportActionBar().hide();
+
+        //getSupportActionBar().hide();
+        //getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources()
+          //      .getColor(R.color.bg_color)));
 
 
-        getWindow().getDecorView().setSystemUiVisibility(
+      /*  getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                       | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                       // | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                         | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                         | View.SYSTEM_UI_FLAG_IMMERSIVE);
+*/
+
 
         title=(TextView)findViewById(R.id.title2);
         quote=(TextView)findViewById(R.id.quote2);
         name=(TextView)findViewById(R.id.name2);
-        bb=(FloatingActionButton) findViewById(R.id.bb);
+        bb=(Button) findViewById(R.id.bb);
+        submit=(Button)findViewById(R.id.email);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SendEmail();
+            }
+        });
 
         LinearLayout layout = (LinearLayout)findViewById(R.id.ll);
-        String a,b,c,d;
+        //ViewGroup.LayoutParams lp = (ViewGroup.LayoutParams) layout.getLayoutParams();
+        //lp.height = layout.getWidth();
+
         SharedPreferences prefs = getSharedPreferences("mysharedPref", MODE_PRIVATE);
         a=prefs.getString("name","name");
         b=prefs.getString("quote","name");
         c=prefs.getString("title","name");
         d=String.format("[%s]",c.toUpperCase());
-        name.setText(a+" ");
+       //String output = a.substring(0, 1).toUpperCase() + a.substring(1);
+       String output= capitalizeFirstLetter(a);
+       name.setText(output+" ");
         quote.setText(b.toUpperCase());
         title.setText(d);
 
@@ -66,7 +95,9 @@ bb.setOnClickListener(new View.OnClickListener() {
     @Override
     public void onClick(View view) {
         bb.setVisibility(View.GONE);
-        bitmap = ScreenShott.getInstance().takeScreenShotOfRootView(view);
+        submit.setVisibility(View.GONE);
+        View v = findViewById(R.id.ss);
+        bitmap = getScreenshot(v);
         if (bitmap != null) {
             if (hasWritePermission) {
                 saveScreenshot();
@@ -76,18 +107,46 @@ bb.setOnClickListener(new View.OnClickListener() {
             }
         }
         bb.setVisibility(View.VISIBLE);
+        submit.setVisibility(View.VISIBLE);
 
     }
 });
 
     }
 
+    public static Bitmap getScreenshot(View v){
+        v.setDrawingCacheEnabled(true);
+        Bitmap bitmap = Bitmap.createBitmap(v.getDrawingCache());
+        v.setDrawingCacheEnabled(false);
+        return bitmap;
+
+    }
+    public String capitalizeFirstLetter(String original) {
+        if (original == null || original.length() == 0) {
+            return original;
+        }
+        return original.substring(0, 1).toUpperCase() + original.substring(1);
+    }
+    private void SendEmail() {
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"Theprojectquote@gmail.com"});
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Send quotes");
+        intent.putExtra(Intent.EXTRA_TEXT, "Title- "+c+"\n\nQuote- "+b
+                +"\n\nName- "+a);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+
+        }
+
+    }
     private void saveScreenshot() {
         // Save the screenshot
 
         try {
             File file = ScreenShott.getInstance()
-                    .saveScreenshotToPicturesFolder(Main2Activity.this, bitmap, "my_screenshot");
+                    .saveScreenshotToPicturesFolder(Main2Activity.this, bitmap, "TheProjectQuote");
             // Display a toast
             Toast.makeText(Main2Activity.this, "Image Saved" /*+ file.getAbsolutePath()*/,
                     Toast.LENGTH_SHORT).show();
--- app/src/main/java/com/the/project/quotes/MainActivity.java
+++ app/src/main/java/com/the/project/quotes/MainActivity.java
@@ -12,42 +12,42 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends AppCompatActivity {
     private EditText inputTitle, inputQuote, inputName;
-    private Button submit,email;
-    private TextInputLayout inputLayoutTitle, inputLayoutQuote, inputLayoutName;
+    private Button submit, email;
+    //private TextInputLayout inputLayoutTitle, inputLayoutQuote, inputLayoutName;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        submit=(Button)findViewById(R.id.submit);
-        email=(Button)findViewById(R.id.email);
-
-        inputLayoutTitle = (TextInputLayout) findViewById(R.id.input_layout_title);
-        inputLayoutQuote = (TextInputLayout) findViewById(R.id.input_layout_quote);
-        inputLayoutName = (TextInputLayout) findViewById(R.id.input_layout_name);
+        submit = (Button) findViewById(R.id.submit);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.mipmap.ic_launcher);
+        // inputLayoutQuote = (TextInputLayout) findViewById(R.id.input_layout_quote);
+        //inputLayoutName = (TextInputLayout) findViewById(R.id.input_layout_name);
         inputTitle = (EditText) findViewById(R.id.input_title);
         inputQuote = (EditText) findViewById(R.id.input_quote);
         inputName = (EditText) findViewById(R.id.input_name);
-        inputTitle.addTextChangedListener(new MyTextWatcher(inputTitle));
-        inputQuote.addTextChangedListener(new MyTextWatcher(inputQuote));
+        textView = (TextView) findViewById(R.id.result);
+        //inputTitle.addTextChangedListener(new MyTextWatcher(inputTitle));
+        //inputQuote.addTextChangedListener(new MyTextWatcher(inputQuote));
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 submitForm();
             }
         });
-        email.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SendEmail();
-            }
-        });
 
     }
+
+    /*
     private class MyTextWatcher implements TextWatcher {
 
         private View view;
@@ -74,42 +74,26 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }}
+        */
     private void requestFocus(View view) {
         if (view.requestFocus()) {
             getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
         }
     }
+
     private boolean validateTitle() {
-        if (inputTitle.getText().length()>40) {
-            inputLayoutTitle.setError("Length should be less than 40 characters");
+        if (inputTitle.getText().length() > 40) {
+            textView.setText("Length should be less than 40 characters");
             requestFocus(inputTitle);
             return false;
-        } else {
-            inputLayoutTitle.setErrorEnabled(false);
+        } else if (inputTitle.getText().toString().contains(" ")) {
+            textView.setText("Title should not contain spaces");
+            requestFocus(inputTitle);
+            return false;
         }
-
         return true;
     }
-    private void SendEmail() {
-        if (!validateTitle()) {
-            return;
-        }
 
-        if (!validateQuote()) {
-            return;
-        }
-        Intent intent = new Intent(Intent.ACTION_SENDTO);
-        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
-        intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"Theprojectquote@gmail.com"});
-        intent.putExtra(Intent.EXTRA_SUBJECT, "Send quotes");
-        intent.putExtra(Intent.EXTRA_TEXT, "Title- "+inputTitle.getText().toString()+"\n\nQuote- "+inputQuote.getText().toString()
-        +"\n\nName- "+inputName.getText().toString());
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-
-        }
-
-            }
     private void submitForm() {
         if (!validateTitle()) {
             return;
@@ -118,25 +102,26 @@ public class MainActivity extends AppCompatActivity {
         if (!validateQuote()) {
             return;
         }
-
+        textView.setText("");
         SharedPreferences.Editor editor = getSharedPreferences("mysharedPref", MODE_PRIVATE).edit();
         editor.putString("name", inputName.getText().toString());
-        editor.putString("quote",inputQuote.getText().toString());
+        editor.putString("quote", inputQuote.getText().toString());
         editor.putString("title", inputTitle.getText().toString());
         editor.apply();
 
 
         Intent myIntent = new Intent(MainActivity.this, Main2Activity.class);
-        startActivity(myIntent);    }
+        startActivity(myIntent);
+    }
+
     private boolean validateQuote() {
-        if (inputQuote.getText().length()>300) {
-            inputLayoutQuote.setError("Length should be less than 300 characters");
+        if (inputQuote.getText().length() > 140) {
+            textView.setText("Length should be less than 140 characters");
             requestFocus(inputQuote);
             return false;
-        } else {
-            inputLayoutQuote.setErrorEnabled(false);
         }
 
         return true;
     }
+
 }
